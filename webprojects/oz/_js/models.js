51aece2b8919746732803d10a46b6c88afc01934
Sammy Jelin <sjelin@gmail.com>
2014-01-01 01:23:08 -0500

    made payments an array

--- webprojects/oz/_js/models.js
+++ webprojects/oz/_js/models.js
@@ -1,7 +1,25 @@
 /**	This file handels the models for what is basically the most basic MVC
  *	ever.  We keep track of the variables for each ticket:
- *		items - The items on the tables.
- *		payments -	The way the items are split between the payers.
+ *		items - The items on the tables.  Follow the TicketItem.java format
+ *		payments -	The way the items are split between the payers.  Each
+ *					payment has the following elements:
+ *						tKey - The key for the table
+ *						cID - The connection ID of the payer
+ *						itemsToPay - The items being targeted by this payment
+ *						payFracNums - Numerators of the fractions to pay
+ *						payFracDenoms - Denominators of the fractions to pay
+ *						total - The total which the payer has been shown
+ *						tip - How much the payer tipped
+ *						pan - Card information
+ *						name - Card information
+ *						expr - Card information
+ *						zip - Card information
+ *						cvv - Card information (optional)
+ *						focus - True iff this payment has the focus
+ *						notification - True iff payment hasn't been viewed
+ *						timeStamp - When the payment came in
+ *						statusCode - The status of the payment
+ *						statusMsg - The message for the payer
  *
  *	The following methods are used to access these variables:
  *		models.refresh()
@@ -34,7 +52,12 @@ models = models || {};
 	var tickets;
 	var payments = {};
 
-	models.refresh = function(callback) {
+	/** Initializes the app, but does not clear any payment info in RAM
+	 *
+	 *	Sets up a new connection with the server, downloads the current
+	 *	tickets, opens a socket, and calls drawBase() and drawTick()
+	 */
+	models.refresh = function() {
 		post("connect", {restr: restr}, function(data) {
 			data = JSON.parse(data);
 			openSocket(data.token);
@@ -46,12 +69,20 @@ models = models || {};
 				tickets = data.ticks;
 			drawBase(Object.keys(tickets));
 			for(tKey in tickets) {
-				payments[tKey] = payments[tKey] || {};
+				if(payments[tKey] == undefined) {
+					payments[tKey] =  [];
+					payments[tKey].byCID = {};
+				}
 				drawTick(tKey, tickets[tKey], payments[tKey]);
 			}
 		});
 	}
 
+	/*	Opens a socket to the server & sets up the socket events
+	 *
+	 *	Once this is done, any payments sent to the server will automatically
+	 *	be pushed to this console
+	 */
 	function openSocket(token)
 	{
 		skt = (new goog.appengine.Channel(token)).open({
@@ -65,10 +96,11 @@ models = models || {};
 			payment = JSON.parse(payment);
 			payment.focus = false;
 			patment.notification = true;
-			payment.statusCode = models.STATUS_NONE;i
+			payment.statusCode = models.STATUS_NONE;
 			payment.statusMsg = "";
 			payment.timeStamp = new Date();
-			payments[payment.tKey][payment.cID] = payment;
+			payments[payment.tKey].push(payment);
+			payments[payment.tKey].byCID[payment.cID] = payment;
 		};
 		skt.onerror = function(err) {
 			alert("SOCKET ERROR!  Please restart.\n\n"+JSON.stringify(err));
@@ -86,6 +118,11 @@ models = models || {};
 		};
 	}
 
+	/**	Sets the items on a ticket and calls drawTick
+	 *
+	 *	@param	tKey The key of the ticket to set the items of
+	 *	@param	items The items for the ticket
+	 */
 	models.setItems = function(tKey, items) {
 		tickets[tKey] = items;
 		send("tick", {restr: restr,	i: parseInt(tKey.substr(2)),
@@ -93,19 +130,34 @@ models = models || {};
 		drawTick(tKey, items, payments[tKey]);
 	};
 
+	/**	Gives/takes the focus to/from particular payment and calls drawTick
+	 *
+	 *	@param	tKey The ticket which the payment is in
+	 *	@param	cID The connection ID of the payer
+	 *	@param	focus If the payment should have the focus
+	 */
 	models.setPaymentFocus = function(tKey, cID, focus) {
 		var tPayments = payments[tKey];
 		if(focus) {
-			tPayments[cID].notification = false;
-			for(var i in tPayments)
-				tPayments[i].focus = i == cID;
+			tPayments.byCID[cID].notification = false;
+			for(var i in tPayments.byCID)
+				tPayments.byCID[i].focus = i == cID;
 		} else
-			tPayments[cID].focus = false;
+			tPayments.byCID[cID].focus = false;
 		drawTick(tKey, tickets[tKey], tPayments);
 	};
 
+	/**	Sets the status a payment and updates the payer on that status.
+	 *
+	 *	Also calls drawTick
+	 *
+	 *	@param	tKey The key of the table to which the payment belongs
+	 *	@param	cID The connection ID of the payer
+	 *	@param	statusCode The status code of the update
+	 *	@param	statusMsg The message for the update
+	 */
 	models.setPaymentStatus = function(tKey, cID, statusCode, statusMsg) {
-		var payment = payments[tKey][cID];
+		var payment = payments[tKey].byCID[cID];
 		payment.statusCode = statusCode;
 		payment.statusMsg = statusMsg;
 		if(statusCode != 0)
