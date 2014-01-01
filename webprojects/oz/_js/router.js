c3cce1eeb0e1fc8705c0ac057ccf441c42ba00c0
Sammy Jelin <sjelin@gmail.com>
2014-01-01 01:20:30 -0500

    finished oz router

--- webprojects/oz/_js/router.js
+++ webprojects/oz/_js/router.js
@@ -5,10 +5,13 @@ $(document).ready(models.refresh);
 
 window.onhashchange = function() {
 	var cmd = (location.hash||"").slice(1);
+	$(".nav .tBtn").removeClass("focus");
+	$(".tickets > .ticket").hide()
 	if(cmd.startsWith("tick-")) {
 		var tKey = cmd.substr(5);
-	} else
-		drawClear();
+		$(".nav .tBtn[tKey="+tKey+"]").removeClass("focus");
+		$(".tickets > .ticket[tKey="+tKey+"]").show()
+	}
 	if(cmd == "refresh" || cmd == "reload" || cmd == "reconnect")
 		models.refresh();
 }
